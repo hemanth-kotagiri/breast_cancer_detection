b3b9bd43cb0c30e9843b690b6dd749bf61ad9dfe
Hemanth <hemanth.kotagiri43@gmail.com>
2020-07-01 12:43:49 +0530

    Add Logistic Regression on Local Data

--- Model Building.py
+++ Model Building.py
@@ -16,3 +16,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model.fit(X_train, y_train)
 
 print(precision_score(y_test, model.predict(X_test)))
+
+
+data_df = pd.read_csv("Data/data.csv")
+
+y = data_df["diagnosis"].apply(lambda x: 1 if x == 'M' else 0)
+# print(data_df.columns)
+
+X_train, X_test, y_train, y_test = train_test_split(data_df[["radius_mean", "smoothness_mean", "radius_worst", "smoothness_worst"]], y, test_size = 0.2)
+
+model.fit(X_train, y_train)
+
+print(precision_score(y_test, model.predict(X_test)))
