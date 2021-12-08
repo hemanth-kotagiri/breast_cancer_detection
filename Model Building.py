ac5dca54b12886da0155c22706247c489bc0847a
Hemanth <hemanth.kotagiri43@gmail.com>
2021-12-08 20:15:45 +0530

    Update gitignore

--- Model Building.py
+++ Model Building.py
@@ -18,18 +18,21 @@ y = data_df["diagnosis"].apply(lambda x: 1 if x == 'M' else 0)
 # print(data_df.columns)
 
 
-features = ['radius_mean', 'texture_mean', 'perimeter_mean',   
-       'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
-       'radius_worst', 'texture_worst',
-       'perimeter_worst', 'area_worst', 'smoothness_worst',
-       'compactness_worst', 'concavity_worst', 'concave points_worst',       
-       'symmetry_worst', 'fractal_dimension_worst']
+features = ['radius_mean', 'texture_mean', 'perimeter_mean',
+            'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
+            'radius_worst', 'texture_worst',
+            'perimeter_worst', 'area_worst', 'smoothness_worst',
+            'compactness_worst', 'concavity_worst', 'concave points_worst',
+            'symmetry_worst', 'fractal_dimension_worst']
 
-X_train, X_test, y_train, y_test = train_test_split(data_df[features], y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(
+    data_df[features], y, test_size=0.2)
 
 
-models = [LogisticRegression(random_state=0, max_iter=5000), RandomForestClassifier(), SVC()]
+models = [LogisticRegression(
+    random_state=0, max_iter=5000), RandomForestClassifier(), SVC()]
 
-for i,model in enumerate(models, start = 1):
+for i, model in enumerate(models, start=1):
     model.fit(X_train, y_train)
-    print(f"Model {i} Accuracy: ", precision_score(y_test, model.predict(X_test)))
+    print(f"Model {i} Accuracy: ", precision_score(
+        y_test, model.predict(X_test)))
